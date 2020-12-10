750185ff084900d63d70fdae216c987ba7d2d440
htcak <htcakuma@gmail.com>
2020-12-11 00:57:14 +0200

    Add 3 and 4 tasks

---
+++ Task_003/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Task_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currencyAmount = default(double);
+            double currencyCourse = default(double);
+            string currencyName = default;
+
+            SetData(ref currencyAmount, ref currencyCourse, ref currencyName);
+
+            Console.WriteLine($"Your want convert to {currencyName} - {currencyAmount}");
+            Console.WriteLine($"In {currencyName} it equal {CalculateCurrency(currencyAmount,currencyCourse)}");
+
+            // Delay
+            Console.ReadLine();
+        }
+
+        static void SetData(ref double currencyAmount, ref double currencyCourse, ref string currencyName) 
+        {
+            bool isCorrectNumber = false;
+
+            Console.WriteLine("Hello, Please indicate the name of the currency in which you want to change your current... ");
+            currencyName = Console.ReadLine();
+                
+            while (!isCorrectNumber)
+            {
+                Console.WriteLine($"Please type how much {currencyName} do you want to change?");
+                String userInputString = Console.ReadLine();
+                bool isConvertCorrect = double.TryParse(userInputString, out currencyAmount);
+                if (isConvertCorrect)
+                {
+                    currencyAmount = double.Parse(userInputString);
+                    if (currencyAmount <= 0) { Console.WriteLine("You cant convert less than 0 or 0"); } 
+                    isCorrectNumber = true;
+                }
+                else Console.WriteLine("Input error, please try again ");
+            }
+
+            isCorrectNumber = false;
+
+            while (!isCorrectNumber)
+            {
+                Console.WriteLine($"Hi, Please type the {currencyName} currency rate");
+                String userInputString = Console.ReadLine();
+                bool isConvertCorrect = double.TryParse(userInputString, out currencyCourse);
+                if (isConvertCorrect)
+                {
+                    currencyCourse = double.Parse(userInputString);
+                    if (currencyCourse <= 0) { Console.WriteLine("currency rate cannot be less than or equal to 0"); }
+                    isCorrectNumber = true;
+                }
+                else Console.WriteLine("Input error, please try again ");
+            }
+        }
+        static double CalculateCurrency(double currencyAmount, double currencyCourse)
+        {
+            return currencyAmount * currencyCourse;
+        }
+    }
+}
---
+++ Task_004/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Task_004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userValue = default;
+            int[] arr = new int[] { 2, 5, 3, 6, 9};
+            SetUserValue(ref userValue);
+            Console.WriteLine(
+                isValuePositive(userValue) == true ? "Число Положительно" :
+                isValueNEgative(userValue) == true ? "Число Негативно" :
+                "Число ноль");
+
+            Console.WriteLine(isValueSimple(userValue) == true ? "Число Простое\n" : "Число составное\n");
+            CheckDivision(userValue, arr);
+
+
+            // Delay
+            Console.ReadLine();
+
+        }
+
+        static void SetUserValue(ref int userValue)
+        {
+            bool isCorrect = false;
+
+            while (!isCorrect)
+            {
+                Console.WriteLine("Type a number...");
+                string userInput = Console.ReadLine();
+
+                bool isConvertCorrect = int.TryParse(userInput, out userValue);
+                if (isConvertCorrect)
+                {
+                    userValue = int.Parse(userInput);
+                    isCorrect = true;
+                }
+                else Console.WriteLine("Input error, please try again ");
+            }
+        }
+        static bool isValuePositive(int value)
+        {
+            return value > 0 ? true : false;
+        }
+
+        static bool isValueNEgative(int value)
+        {
+            return value < 0 ? true : false;
+        }
+
+        static bool isValueSimple(int value)
+        {
+            for (int i = 2; i <= Math.Sqrt(value); i++)
+                if (value % i == 0)
+                    return false;
+            return true;
+        }
+
+        static void CheckDivision(int value, int[] values) 
+        {
+            foreach (var item in values) 
+            {
+                if (value % item == 0) 
+                {
+                    
+                    Console.WriteLine($"Число {value} делится на {item} без остатка");
+                }
+                else Console.WriteLine($"Число {value} делится на {item} c остатком {value%item}");
+            }
+        }
+    }
+}
